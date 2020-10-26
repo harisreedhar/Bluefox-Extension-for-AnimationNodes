943da1dc8d3d2f1f4972a96064bc103fc8e141ba
Harisreedhar <harisreedhar@users.noreply.github.com>
2020-10-26 10:14:47 +0530

    add effector nodes

--- an_bluefox_extension/nodes/test/test_utils.pyx
+++
@@ -1,9 +0,0 @@
-import cython
-from animation_nodes.data_structures cimport DoubleList
-
-def addOneList(DoubleList num):
-    cdef DoubleList result = DoubleList(length = num.length)
-    cdef int i
-    for i in range(num.length):
-        result.data[i] = num.data[i] + 1
-    return result
--- an_bluefox_extension/ui/node_menu.py
+++ an_bluefox_extension/ui/node_menu.py
@@ -7,7 +7,8 @@ class BluefoxExtensionMenu(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        insertNode(layout, "an_ColorComposite", "Color Composite")
+        layout.menu("AN_MT_Colors_menu", text = "Color")
+        layout.menu("AN_MT_Effectors_menu", text = "Effectors")
 
 def drawMenu(self, context):
     if context.space_data.tree_type != "an_AnimationNodeTree": return
@@ -16,7 +17,26 @@ def drawMenu(self, context):
     layout.operator_context = "INVOKE_DEFAULT"
 
     layout.separator()
-    layout.menu("AN_MT_bluefox_extension_menu", text = "Bluefox Extension Menu", icon = "SCRIPTPLUGINS")
+    layout.menu("AN_MT_bluefox_extension_menu", text = "Bluefox Extension Menu", icon = "MESH_MONKEY")
+
+class ColorMenu(bpy.types.Menu):
+    bl_idname = "AN_MT_Colors_menu"
+    bl_label = "Colors Menu"
+
+    def draw(self, context):
+        layout = self.layout
+        insertNode(layout, "an_ColorComposite", "Color Composite")
+
+class EffectorMenu(bpy.types.Menu):
+    bl_idname = "AN_MT_Effectors_menu"
+    bl_label = "Effectors Menu"
+
+    def draw(self, context):
+        layout = self.layout
+        insertNode(layout, "an_Inheritanceffector", "Inheritance Effector")
+        insertNode(layout, "an_StepEffector", "Step Effector")
+        insertNode(layout, "an_TargetEffector", "Target Effector")
+        insertNode(layout, "an_TimeEffector", "Time Effector")
 
 def register():
     bpy.types.NODE_MT_add.append(drawMenu)

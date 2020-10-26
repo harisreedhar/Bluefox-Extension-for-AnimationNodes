403a9ab345cb5a1f6195b21de393eabc8b6c3783
Harisreedhar <harisreedhar@users.noreply.github.com>
2020-10-26 10:53:14 +0530

    Ported nodes from AN_Bluefox branch

--- an_bluefox_extension/ui/node_menu.py
+++ an_bluefox_extension/ui/node_menu.py
@@ -9,6 +9,21 @@ class BluefoxExtensionMenu(bpy.types.Menu):
         layout = self.layout
         layout.menu("AN_MT_Colors_menu", text = "Color")
         layout.menu("AN_MT_Effectors_menu", text = "Effectors")
+        layout.menu("AN_MT_extrafalloffs_menu", text = "Extra Falloffs")
+        layout.menu("AN_MT_Miscellaneous_menu", text = "Miscellaneous")
+        insertNode(layout, "an_SverchokInterfaceNode", "Sverchok Interface")
+        insertNode(layout, "an_SimpleDeformNode", "Simple Deform")
+        insertNode(layout, "an_SwitchListElementsNode", "Switch List Elements")
+        insertNode(layout, "an_RigidBodyTrigger", "Rigid Body Trigger")
+        insertNode(layout, "an_MarchingCubes", "Marching Cubes")
+        insertNode(layout, "an_BMeshSmoothNode", "BMesh Smooth")
+        insertNode(layout, "an_DupliInstancer", "Dupli Instancer")
+        insertNode(layout, "an_splinetracer", "Spline Tracer")
+        insertNode(layout, "an_MemoryNode", "Memory Node")
+        insertNode(layout, "an_Memoryfalloff", "Memory Falloff")
+        insertNode(layout, "an_Colormix2", "Color Mix 2")
+        insertNode(layout, "an_CompositeFalloffsNode", "Composite Falloff")
+        insertNode(layout, "an_NormalizeFloatsNode", "Normalize Floats")
 
 def drawMenu(self, context):
     if context.space_data.tree_type != "an_AnimationNodeTree": return
@@ -17,7 +32,7 @@ def drawMenu(self, context):
     layout.operator_context = "INVOKE_DEFAULT"
 
     layout.separator()
-    layout.menu("AN_MT_bluefox_extension_menu", text = "Bluefox Extension Menu", icon = "MESH_MONKEY")
+    layout.menu("AN_MT_bluefox_extension_menu", text = "Bluefox Nodes", icon = "MESH_MONKEY")
 
 class ColorMenu(bpy.types.Menu):
     bl_idname = "AN_MT_Colors_menu"
@@ -37,6 +52,27 @@ class EffectorMenu(bpy.types.Menu):
         insertNode(layout, "an_StepEffector", "Step Effector")
         insertNode(layout, "an_TargetEffector", "Target Effector")
         insertNode(layout, "an_TimeEffector", "Time Effector")
+
+class ExtrafalloffsMenu(bpy.types.Menu):
+    bl_idname = "AN_MT_extrafalloffs_menu"
+    bl_label = "Extra falloffs Menu"
+
+    def draw(self, context):
+        layout = self.layout
+        insertNode(layout, "an_Formulafalloff", "Formula Falloff")
+        insertNode(layout, "an_RadialFalloff_old", "Radial Falloff(old)")
+        insertNode(layout, "an_wavefalloff", "Wave falloff")
+
+class MiscellaneousMenu(bpy.types.Menu):
+    bl_idname = "AN_MT_Miscellaneous_menu"
+    bl_label = "Miscellaneous Menu"
+
+    def draw(self, context):
+        layout = self.layout
+        insertNode(layout, "an_CurlNoise", "Curl Noise")
+        insertNode(layout, "an_Spherical_spiral", "Spherical spiral")
+        insertNode(layout, "an_fibonacci", "Fibonacci")
+        insertNode(layout, "an_lorenz", "Lorenz Attractor")                
 
 def register():
     bpy.types.NODE_MT_add.append(drawMenu)
